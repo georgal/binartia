14ea5e1e5f3608c121cd30189a14ca6f3313295f
George <george.georgalidis@gmail.com>
2017-06-17 00:58:05 +0300

    beta
    The art of binary

---
+++ dark.js
@@ -0,0 +1,10 @@
+function write_inside() {
+    //document.getElementById("textarea").innerHTML = "test";
+	window.document.body.style.backgroundColor= "#F5F5F5";
+	var bord = $('ul');
+	bord.css( 'borderColor', 'black');
+	var hidd = $('');
+	hidd.show();
+	
+	
+}
